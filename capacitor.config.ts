624c9c8a6f1fb8275718ae0b8b473a2fab14e520
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2023-11-07 16:35:36 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lab3_ionic',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
